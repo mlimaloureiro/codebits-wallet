d098d329f12193b5d71824597912b2177a36144a
mdiasloureiro <mdiasloureiro@gmail.com>
2014-04-10 19:08:08 +0100

    test get issues from one repository working

--- wallet/walletapp/controllers/dash.py
+++ wallet/walletapp/controllers/dash.py
@@ -7,9 +7,15 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.contrib.auth.decorators import login_required
 import simplejson as json
+import requests
 import datetime
 
 def index(request):
 	opts = {}
 	t = loader.get_template('dash.html')
 	return HttpResponse(t.render(RequestContext(request, opts)))
+
+def get_issues(request):
+	req = requests.get('https://api.github.com/repos/symfony/symfony/issues')
+	return HttpResponse(json.dumps(req.json()), content_type="json")
+
--- wallet/walletapp/urls.py
+++ wallet/walletapp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import *
 from django.contrib.auth import *
 
 urlpatterns = patterns('walletapp.controllers',
-	(r'^$', 'dash.index')
+	(r'^$', 'dash.index'),
+	(r'issues$', 'dash.get_issues'),
 )
+
