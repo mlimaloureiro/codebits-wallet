a1550c035ad72a8825fbf0160039811e07bb243c
mdiasloureiro <mdiasloureiro@gmail.com>
2014-04-10 21:55:35 +0100

    testing some js

--- wallet/wallet/media/js/wallet.js
+++ wallet/wallet/media/js/wallet.js
@@ -1,8 +1,39 @@
+function Repositories(endpoint) {
+
+	this.server = endpoint;
+
+	this.get = function() {
+		console.log("Get from endpoint");
+	};
+
+	this.render = function() {
+		console.log("Rendering repositories");
+	};
+
+}
+
+function Issues(endpoint) {
+	this.server = endpoint;
+
+	this.get = function() {
+		console.log("Get from issues");
+	};
+
+	this.render = function() {
+		console.log("Rendering issues");
+	};
+}
+
 var wallet = {
+
+	endpoint: 'http://localhost:8000', 
+	repositories: new Repositories(this.endpoint),
+	issues: new Issues(this.endpoint),
 
 	init: function() {
 		console.log("wallet app started");
-	},
+		this.repositories.get();
+	}
 
 };
 
