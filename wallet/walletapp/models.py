d4665064613b983c65c8f12f9608273e0744d6b2
mdiasloureiro <mdiasloureiro@gmail.com>
2014-04-10 19:16:48 +0100

    proposition migration

--- wallet/walletapp/models.py
+++ wallet/walletapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Proposition(models.Model):
+    user = models.ForeignKey(User)
+    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
