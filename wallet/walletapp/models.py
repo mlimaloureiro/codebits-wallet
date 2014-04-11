ba01604ffb86dd85e9e8a58a60ea2bf289cba0dc
Gonçalo Valério <gon@ovalerio.net>
2014-04-12 00:53:51 +0100

    aditions to propositions

--- wallet/walletapp/models.py
+++ wallet/walletapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Repositories(models.Model):
     user = models.ForeignKey(User)
     name = models.CharField(max_length=200)
@@ -12,6 +13,22 @@ class Repositories(models.Model):
     hook_url = models.CharField(max_length=300)
     total_propositions = models.IntegerField(default = 0)
 
+
+WAITING = 0
+FAILED = 1
+ACTIVE = 2
+RETURNED = 3
+DONE = 4
+
+PROP_STATUSES = (
+    (WAITING, 'Waiting'),
+    (FAILED, 'Failed'),
+    (ACTIVE, 'Active'),
+    (RETURNED, 'Returned'),
+    (DONE, 'Done'),
+)
+
+
 class Proposition(models.Model):
     user = models.ForeignKey(User)
     repository = models.ForeignKey(Repositories)
@@ -19,6 +36,9 @@ class Proposition(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     issue_id = models.CharField(max_length=300)
     value = models.IntegerField(default=500)
+    status = models.IntegerField(choices=PROP_STATUSES, default=WAITING)
+    expires = models.DateTimeField(null=True, blank=True)
+
 
 class Favourites(models.Model):
     user = models.ForeignKey(User)
