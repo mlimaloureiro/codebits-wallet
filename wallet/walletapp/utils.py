c4b139163180b2659a08176a97359df5852546cd
Gonçalo Valério <gon@ovalerio.net>
2014-04-12 06:40:12 +0100

    pull teste

--- wallet/walletapp/utils.py
+++ wallet/walletapp/utils.py
@@ -2,32 +2,30 @@ from django.conf import settings
 import requests
 import simplejson as json
 
-# def make_payment(user, value):
-
-
-#     data = {
-#         "payment": {
-#             "client": {
-#                 "name": " ".join([]),
-#                 "email": user,
-#                 "address": {
-#                      "country": "pt",
-#                      "address": "some street",
-#                      "city": "lisboa",
-#                      "postalcode": "1100-000"
-#                 }
-#             },
-#             "amount": value/100.0,
-#             "currency": "EUR",
-#             "items": [],
-#         },
-#         "url_confirm": "https://codingbooster.herokuapp.com" + reverse('success_proposition'),
-#         "url_cancel": "https://codingbooster.herokuapp.com" + reverse('failed_proposition')
-#     }
-#     url = "https://services.wallet.codebits.eu/api/v2/checkout"
-#     headers = {
-#         "Authorization": "WalletPT "+settings.WALLET_MER_ID,
-#         'content-type': 'application/json'
-#     }
-#     payload = json.dumps(data)
-#     response = requests.post(url, data=payload, headers=headers)
+def make_payment(user, value):
+    data = {
+        "payment": {
+            "client": {
+                "name": " ".join([]),
+                "email": user,
+                "address": {
+                     "country": "pt",
+                     "address": "some street",
+                     "city": "lisboa",
+                     "postalcode": "1100-000"
+                }
+            },
+            "amount": value/100.0,
+            "currency": "EUR",
+            "items": [],
+        },
+        "url_confirm": "https://codingbooster.herokuapp.com",
+        "url_cancel": "https://codingbooster.herokuapp.com"}
+    url = "https://services.wallet.codebits.eu/api/v2/checkout"
+    headers = {
+        "Authorization": "WalletPT "+settings.WALLET_MER_ID,
+        'content-type': 'application/json'
+    }
+    payload = json.dumps(data)
+    response = requests.post(url, data=payload, headers=headers)
+    return response
